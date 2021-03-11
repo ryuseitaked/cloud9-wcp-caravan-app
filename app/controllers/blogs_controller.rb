8a7714c1b99afa53ee285852b56f6995526052c2
ryuseitaked <ryutake0306@gmail.com>
2021-03-11 02:46:42 +0000

    [start]3-5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,7 @@
 class BlogsController < ApplicationController
   def index
+    @blogs=Blog.all
+
   end
 
   def show
@@ -20,7 +22,7 @@ class BlogsController < ApplicationController
 
   private
   def blog_params
-    params.require(:blog).permit(:title,category,:body)
+    params.require(:blog).permit(:title,:category,:body)
   end
 
 end
